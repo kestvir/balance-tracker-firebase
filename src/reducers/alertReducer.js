d2e0e16737da3a01b86e2274d1255398d2a2db54
kestvir <v.kestutis123@gmail.com>
2020-09-07 23:12:03 +0300

    further missing payloads added

--- src/reducers/alertReducer.js
+++ src/reducers/alertReducer.js
@@ -1,17 +1,16 @@
 export const alertReducer = (state, action) => {
-    switch (action.type) {
-        case 'SET_ERROR':
-            return {
-                ...state,
-                errorMessage: { message: action.errorMessage }
-            }
-        case 'SET_SUCCESS':
-            return {
-                ...state,
-                successMessage: { message: action.successMessage }
-            }
-        default:
-            return state
-
-    }
-}
+  switch (action.type) {
+    case "SET_ERROR":
+      return {
+        ...state,
+        errorMessage: action.payload,
+      };
+    case "SET_SUCCESS":
+      return {
+        ...state,
+        successMessage: action.payload,
+      };
+    default:
+      return state;
+  }
+};
